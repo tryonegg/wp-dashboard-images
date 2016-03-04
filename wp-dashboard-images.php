c87e0b55ef4739d3a2ee941b3c551c6ffbf345ca
Tryon Eggleston <tegglest@monmouth.edu>
2016-03-04 09:31:16 -0500

    init

---
+++ wp-dashboard-images.php
@@ -0,0 +1,77 @@
+<?php
+/*
+Plugin Name: Dashboard Featured Images
+Version: 1.0
+Plugin URI: 
+Description: Shows featured images on the dashboard
+Author: Tryon Eggleston
+*/
+
+class wpDashboardImages {
+
+	function __construct(){
+		add_action('init', array( &$this, 'init' ), 20);
+		add_action('admin_head', array( &$this, 'css' ) );
+	}
+
+	function init(){
+		$args = array(
+			'public'   => true,
+		);
+
+		$post_types = get_post_types( $args ); 
+
+		foreach($post_types as $type){
+			add_filter( 'manage_' . $type . 's_columns', array( &$this, 'AddThumbColumn' ) );
+			add_action( 'manage_' . $type . 's_custom_column', array( &$this, 'AddThumbValue' ), 10, 2 );
+		}
+	}
+
+	function css() {
+		echo '
+			<style type="text/css">
+				.column-thumbnail, 
+				.column-phonethumbnail{
+					width:80px;
+				}
+				
+				.column-thumbnail img,
+				.column-phonethumbnail img{
+					width:60px;
+					height:auto;
+				}
+			</style>
+		';
+	}
+
+	function AddThumbColumn($cols) {
+		$cols['thumbnail'] = __('Featured Image');
+
+		return $cols;
+	}
+
+	function AddThumbValue($column_name, $post_id) {
+		$width = (int) 60;
+		$height = (int) 60;
+		
+		if ( 'thumbnail' == $column_name ) {
+			// thumbnail of WP 2.9
+
+			if('thumbnail' == $column_name){
+				$thumbnail_id = get_post_meta( $post_id, '_thumbnail_id', true );
+
+			// image from gallery
+			$attachments = get_children( array('post_parent' => $post_id, 'post_type' => 'attachment', 'post_mime_type' => 'image') );
+			if ($thumbnail_id){
+				$img =  wp_prepare_attachment_for_js( $thumbnail_id );
+				echo  "<a href='" . get_edit_post_link($thumbnail_id) . "'>" . wp_get_attachment_image( $thumbnail_id, 'medium', true ) . "</a>";
+				echo "<br/> ".$img['sizes']['full']['width'] . "x" . $img['sizes']['full']['height'];
+			} else {
+				echo __('');
+			}
+		}
+	}
+
+
+}
+new wpDashboardImages();
